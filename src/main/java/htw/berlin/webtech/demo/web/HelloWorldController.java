05c2a6cf073f424f98f70519e3db1184847dcbae
Malik Kouyaté <Malik.Kouyate@student.htw-berlin.de>
2021-11-02 18:20:39 +0100

    1234

--- src/main/java/htw/berlin/webtech/demo/web/HelloWorldController.java
+++ src/main/java/htw/berlin/webtech/demo/web/HelloWorldController.java
@@ -1,12 +1,17 @@
 package htw.berlin.webtech.demo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
+@RestController
 public class HelloWorldController {
 
     @GetMapping(path = "/")
     public ModelAndView showHelloWorld(){
+
         return new ModelAndView("helloworld");
     }
 }
