989820d75246c50da8246addc228212f286e7fc6
bartu <bartu.oeztuerk@student.htw-berlin.de>
2021-11-09 15:58:46 +0100

    test

--- src/main/java/htw/berlin/webtech/demo/DemoApplication.java
+++ src/main/java/htw/berlin/webtech/demo/DemoApplication.java
@@ -1,9 +1,17 @@
 package htw.berlin.webtech.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
+//@Controller
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = "java/htw/berlin/webtech/demo/web")
 public class DemoApplication {
 
 	public static void main(String[] args) {
