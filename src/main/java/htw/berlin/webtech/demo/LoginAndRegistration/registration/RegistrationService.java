d4054b28d2230fff6b7b69aa1f97b3856c921c11
Malik Kouyaté <Malik.Kouyate@student.htw-berlin.de>
2021-12-06 13:36:59 +0100

    Email validieren, noch nicht abgeschlossen

--- src/main/java/htw/berlin/webtech/demo/LoginAndRegistration/registration/RegistrationService.java
+++ src/main/java/htw/berlin/webtech/demo/LoginAndRegistration/registration/RegistrationService.java
@@ -1,11 +1,22 @@
 package htw.berlin.webtech.demo.LoginAndRegistration.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
 
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+
+        return "it works";
     }
 }
