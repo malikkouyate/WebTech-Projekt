deac27df4686a4b9df1dfeceb8c3dd22549fe060
bartu <bartu.oeztuerk@student.htw-berlin.de>
2021-12-16 22:57:33 +0100

    test

--- src/main/java/htw/berlin/webtech/demo/WebConfig.java
+++ src/main/java/htw/berlin/webtech/demo/WebConfig.java
@@ -1,4 +1,4 @@
-package htw.berlin.webtech.demo.LoginAndRegistration.security.config;
+package htw.berlin.webtech.demo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
