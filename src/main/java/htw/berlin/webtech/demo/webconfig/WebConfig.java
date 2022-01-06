64c7d2398a8f001ac25aabafcc9c290c0871125f
Malik Kouyaté <Malik.Kouyate@student.htw-berlin.de>
2022-01-06 13:22:16 +0100

    package name anders aufeinmal

--- src/main/java/htw/berlin/webtech/demo/webconfig/WebConfig.java
+++ src/main/java/htw/berlin/webtech/demo/webconfig/WebConfig.java
@@ -1,4 +1,4 @@
-package htw.berlin.webtech.demo.Webconfig;
+package htw.berlin.webtech.demo.webconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
